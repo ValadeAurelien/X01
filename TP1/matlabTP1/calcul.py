b2696fc31ca4266891e4780dee23e5ba3b657634
ValadeAurelien <valade.aurelien69580@gmail.com>
2017-10-04 22:36:19 +0200

    rapport, calcul.py et zip

--- TP1/matlabTP1/calcul.py
+++ TP1/matlabTP1/calcul.py
@@ -14,27 +14,4 @@ f = u - diff(A, x) * diff(u, x) + diff(A, y) * diff(u, y) - A *( diff(diff(u, x)
 #print(f)
 print(str(simplify(f)).replace(")*c", ").*c").replace(")*s", ").*s").replace("**", "^"))
 
-##
-
-from sympy import *
-#A = [ [ a , b ], \
-#      [ c , d ]]
-#telle que A(X-B) renvoie sur le triangle unité 
-
-a = Symbol('a')
-b = Symbol('b')
-c = Symbol('c')
-d = Symbol('d')
-
-x1 = Symbol('x1')
-y1 = Symbol('y1')
-x2 = Symbol('x2')
-y2 = Symbol('y2')
-x3 = Symbol('x3')
-y3 = Symbol('y3')
-
-ToSolve = Matrix( ( (x2-x1, y2-y1, 0, 0, 1), (0, 0, x2-x1, y2-y1, 0), (x3-x1, y3-y1, 0, 0, 0), (0, 0, x3-x1, y3-y1, 1) ) )
-system = ToSolve[:,:-1], ToSolve[:,-1]
-A = linsolve(system, a, b, c, d)
-print(XX)
 
